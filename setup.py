5ec1aef22760ddb41c3b447ad6693207cf2a345a
Avinash Kadaji <kadaji@gmail.com>
2025-12-25 08:13:33 -0500

    updated manifest file

--- setup.py
+++ setup.py
@@ -1,45 +1,51 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
     name="django-ses-tracking",
-    version="1.0.3",
+    version="3.3.0",
     author="Canusia",
     author_email="info@canusia.com",
     description="Django app for tracking AWS SES email events (bounces, complaints, deliveries)",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/Canusia/package_ses_tracking",
+    
+    # Package is at root level
     packages=['ses_tracking'],
     package_dir={'ses_tracking': '.'},
-    package_data={
-        'ses_tracking': [
-            'migrations/*.py',
-            'migrations/__pycache__/*',
-            'templates/ses_tracking/daily_stats/*.html',
-            'templates/ses_tracking/bounces_complaints/*.html',
-        ],
-    },
+    
+    # Include files specified in MANIFEST.in
     include_package_data=True,
+    
     zip_safe=False,
+    
     classifiers=[
         "Development Status :: 4 - Beta",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
-        "Programming Language :: Python :: 3.11",
+        "Environment :: Web Environment",
         "Framework :: Django",
         "Framework :: Django :: 3.2",
         "Framework :: Django :: 4.0",
         "Framework :: Django :: 4.1",
         "Framework :: Django :: 4.2",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Communications :: Email",
     ],
+    
     python_requires=">=3.8",
+    
     install_requires=[
         "Django>=3.2",
         "boto3>=1.26.0",
